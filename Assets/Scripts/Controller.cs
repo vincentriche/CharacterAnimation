0aedf4018fcc0fff3a56e747e0e2a6dcf138dd86
Axel <axel.paris69@gmail.com>
2017-12-14 09:23:39 +0100

    Files!

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+
+// TODO
+// Run animation speed to adapt + transition between run and walk
+// Running Jump
+// Inverse Kinematic
+public class Controller : MonoBehaviour
+{
+	public static Controller Instance;
+
+	[Header("Movements")]
+	public float accelerationSpeed = 50000f;
+	public float maxWalkingSpeed = 4.0f;
+	public float maxRunningSpeed = 8.0f;
+	public float maxCrouchingSpeed = 1.0f;
+	public float jumpVelocity = 20000f;
+	public State state;
+	public GameObject spawnCube;
+
+	private Animator animator;
+	private float cap;
+	private Vector3 groundSpeed;
+	private float walkAndRunTransitionSpeed = 2.0f;
+	private Rigidbody m_rigidbody;
+	private float turn;
+
+	private void Awake()
+	{
+		Instance = this;
+		m_rigidbody = GetComponent<Rigidbody>();
+		animator = GetComponent<Animator>();
+		cap = maxWalkingSpeed;
+	}
+
+	private void Update()
+	{
+		UpdateState();
+		SetAnimatorValues();
+	}
+
+	private void FixedUpdate()
+	{
+		Vector3 m = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+		// Ground
+		if (state != State.Jumped)
+		{
+			Move(m);
+		}
+
+		// Jump
+		if (Input.GetKeyDown(KeyCode.Space)
+			&& (state == State.Grounded || state == State.Running))
+		{
+			state = State.Jumped;
+			m_rigidbody.AddRelativeForce(0f, jumpVelocity, 0f);
+		}
+
+		// Spawn Cubes
+		if (Input.GetKeyDown(KeyCode.L))
+		{
+			Vector3 p = transform.position + transform.forward * 2.0f + transform.up * 2.0f;
+			GameObject o = Instantiate(spawnCube, p, Quaternion.identity);
+			o.GetComponent<Rigidbody>().AddForce(transform.forward * 500.0f);
+		}
+
+		groundSpeed = m_rigidbody.velocity;
+		groundSpeed.y = 0.0f;
+	}
+
+	private void Move(Vector3 move)
+	{
+		// Move
+		if (move.x != 0f || move.z != 0f)
+		{
+			m_rigidbody.AddRelativeForce(move * accelerationSpeed * Time.deltaTime);
+		}
+		if (Input.GetAxisRaw("Vertical") == 0f && Input.GetAxisRaw("Horizontal") == 0f)
+		{
+			m_rigidbody.velocity = Vector3.zero;
+		}
+
+		// Cap Speed
+		Vector2 rigidbody_movement = new Vector2(m_rigidbody.velocity.x, m_rigidbody.velocity.z);
+		if (state == State.Grounded)
+			cap = Mathf.Lerp(cap, maxWalkingSpeed, Time.deltaTime * walkAndRunTransitionSpeed);
+		if (state == State.Crouched)
+			cap = Mathf.Lerp(cap, maxCrouchingSpeed, Time.deltaTime * walkAndRunTransitionSpeed);
+		if (state == State.Running)
+			cap = Mathf.Lerp(cap, maxRunningSpeed, Time.deltaTime * walkAndRunTransitionSpeed);
+		if (rigidbody_movement.magnitude > cap)
+		{
+			rigidbody_movement.Normalize();
+			rigidbody_movement *= cap;
+			m_rigidbody.velocity = new Vector3(rigidbody_movement.x, m_rigidbody.velocity.y, rigidbody_movement.y);
+		}
+	}
+
+	private void UpdateState()
+	{
+		if (Input.GetKey(KeyCode.C) && state == State.Grounded)
+			state = State.Crouched;
+		else if (!Input.GetKey(KeyCode.C) && state == State.Crouched)
+			state = State.Grounded;
+
+		if (Input.GetKey(KeyCode.LeftShift) && state == State.Grounded)
+			state = State.Running;
+		else if (!Input.GetKey(KeyCode.LeftShift) && state == State.Running)
+			state = State.Grounded;
+	}
+
+	private void SetAnimatorValues()
+	{
+		float mX = Mathf.Clamp(Input.GetAxisRaw("Mouse X"), -1, 1);
+		if (mX != 0)
+		{
+			turn += mX * 10.0f * Time.deltaTime;
+			turn = Mathf.Clamp(turn, -1.0f, 1.0f);
+		}
+		else
+			turn = Mathf.Lerp(turn, 0.0f, 10.0f * Time.deltaTime);
+		turn += Input.GetAxis("Horizontal");
+		float f = Input.GetAxis("Vertical");
+		if (state != State.Running)
+			f = Mathf.Clamp(f, 0.0f, 0.5f);
+
+		animator.SetFloat("Upspeed", m_rigidbody.velocity.y);
+		animator.SetFloat("Forward", f);
+		animator.SetFloat("Horizontal", turn);
+		animator.SetBool("Grounded", state == State.Grounded);
+		animator.SetBool("Jump", Input.GetKeyDown(KeyCode.Space));
+		animator.SetBool("Crouch", state == State.Crouched ? true : false);
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (state == State.Jumped)
+			state = State.Grounded;
+	}
+}
+
+public enum State
+{
+	Grounded,
+	Running,
+	Jumped,
+	Crouched
+};
---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+	private float x = 0.0f;
+	private float y = 0.0f;
+	private int mouseXSpeedMod = 3;
+	private int mouseYSpeedMod = 2;
+	private float MaxViewDistance = 15f;
+	private float MinViewDistance = 1f;
+	private float desireDistance;
+	private Transform CameraTarget;
+	private bool inverseYAxe = true;
+
+	private void Start()
+	{
+		CameraTarget = transform.parent;
+		Vector3 Angles = transform.eulerAngles;
+		x = Angles.x;
+		y = Angles.y;
+		desireDistance = 9f;
+	}
+
+	private void LateUpdate()
+	{
+		x += Input.GetAxis("Mouse X") * mouseXSpeedMod;
+		if (inverseYAxe == true)
+			y -= Input.GetAxis("Mouse Y") * mouseYSpeedMod;
+		else
+			y += Input.GetAxis("Mouse Y") * mouseYSpeedMod;
+		y = ClampAngle(y, 10, 65);
+		Quaternion rotation = Quaternion.Euler(y, x, 0);
+
+		desireDistance -= Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * 15f * Mathf.Abs(desireDistance);
+		desireDistance = Mathf.Clamp(desireDistance, MinViewDistance, MaxViewDistance);
+		Vector3 position = CameraTarget.position - (rotation * Vector3.forward * desireDistance);
+
+		position = CameraTarget.position - (rotation * Vector3.forward * desireDistance + new Vector3(0, -3.0f, 0));
+
+		transform.rotation = rotation;
+		transform.position = position;
+		CameraTarget.rotation = Quaternion.Lerp(CameraTarget.rotation, Quaternion.Euler(0.0f, rotation.eulerAngles.y, 0.0f), Time.time * mouseXSpeedMod);
+	}
+
+	private static float ClampAngle(float angle, float min, float max)
+	{
+		if (angle < -360)
+			angle += 360;
+		if (angle > 360)
+			angle -= 360;
+		return Mathf.Clamp(angle, min, max);
+	}
+}
